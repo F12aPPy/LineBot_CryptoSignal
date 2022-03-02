f4937f7ca3f7841dc15b2287a387cfc7dfc034e5
Teerapat Lertvuttichaikul <teerapat_lert@Teerapats-MacBook-Pro.local>
2022-03-02 11:50:48 +0700

    first commit

---
+++ Project/Config.py
@@ -0,0 +1,5 @@
+Channel_secret = '55e43a9cd7b55e8a7db8157ab63418c3'
+
+Channel_access_token = 'FTQQENU03I7kL1KESqjfIhBcvSuJLshqT9kk56LQXobEthOOouMoBbpcTZlVzr4BtyOJjU0XxWZ80qnYrNo70QYcK76mANi1BYhzMqMa9/SBaV9rXsQsscjanLzOFsKVyJEmJpcoAwIEaEXuFGrf8AdB04t89/1O/w1cDnyilFU='
+
+basic_id = '@810alcrc'
